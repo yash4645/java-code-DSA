0aaa862bc121696598d87a2a606051d10d255cb3
Yash Raj <yash46071@gmail.com>
2023-08-09 21:40:04 +0530

    use of stringBuilder

---
+++ stringBuilder.java
@@ -0,0 +1,20 @@
+// import java.util.*;
+public class stringBuilder {
+    // to increse the performance of the string we use this
+    // string is immutable thats why we use StringBuilder
+    public static void main(String[] args) {
+
+        StringBuilder str= new StringBuilder("hello");
+        str.append(" world");
+        System.out.println(str);
+        str.setCharAt(0, 'm');
+        System.out.println(str);
+        str.insert(3,'y');
+        System.out.println(str);
+        str.delete(3, 4);
+        System.out.println(str);
+        System.out.println(str.reverse());
+       
+    }
+
+}
