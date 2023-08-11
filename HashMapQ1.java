dcc0950b7711e3d26e6976970626708be8de4110
Yash Raj <yash46071@gmail.com>
2023-08-11 19:36:04 +0530

    find the charecter that occures more then once

---
+++ HashMapQ1.java
@@ -0,0 +1,31 @@
+import java.util.HashMap;
+import java.util.Scanner;
+// find the number of charecters that occure more then once in the given string
+public class HashMapQ1 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter String");
+        String str=sc.nextLine();
+        System.out.println(countNum(str));
+    }
+    static int countNum(String str){
+        HashMap<Character,Integer> hm=new HashMap<>();
+        for(int i=0;i<str.length();i++){
+            char ch=str.charAt(i);
+            if(!hm.containsKey(ch)){
+                hm.put(ch, 1);
+            }else{
+                int currentVal=hm.get(ch);
+                hm.put(ch, currentVal+1);
+            }
+        }
+        int count=0;
+        for(var e:hm.entrySet()){
+            if(e.getValue()>1){
+                count++;
+            }
+        }
+       return count;
+    }
+}
+
