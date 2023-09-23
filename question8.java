c3ce533b9ce712e97d2035aee3461329cece7d69
Yash Raj <yash46071@gmail.com>
2023-09-23 09:34:37 +0530

    java codes

--- question8.java
+++ question8.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 
 public class question8 {
     public static void main(String[] args) {
-        int[] arr={44,22,33,11,23,32};
+        int[] arr={44,22,33,11,23,32,15};
         for(int i=0;i<arr.length;i++){
             for(int j=i+1;j<arr.length;j++){
                 if(Sum(arr[i])>Sum(arr[j])){
