58a9577c04960b4b9c0ae9c39acc7914d178bfea
Yash Raj <yash46071@gmail.com>
2023-09-27 17:48:00 +0530

    print z pattern

---
+++ question15.java
@@ -0,0 +1,16 @@
+public class question15 {
+    // print z pattern
+    public static void main(String[] args) {
+        int n=5;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(n-i==n ||n-i==1 || j==n-1-i){
+                    System.out.print("* ");
+                }else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        } 
+   }
+}
