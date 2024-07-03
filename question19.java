309286407f49b7bdfc32b3f5a51f6c7ad6e99bd2
Yash Raj <yash46071@gmail.com>
2024-07-03 12:24:14 +0530

    cognizant java problem

---
+++ question19.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class question19 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the string");
+        String S=sc.nextLine();
+
+        System.out.println("enter the number");
+        int N=sc.nextInt();
+
+        StringBuilder ans=new StringBuilder();
+        int current=0;
+
+        for(int i=0;i<S.length();i++){
+            int asci=(int) S.charAt(i);
+            current+=asci;
+            if(current<N){
+                ans.append(S.charAt(i));
+            }else{
+                break;
+            }
+        }
+        String output=ans.toString();
+        System.out.println(output);
+        
+        
+    }
+}
